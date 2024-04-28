5af1eb5dc96c5ffc191d990602099e7097c8c64b
endepointe <endepointe@gmail.com>
2024-04-28 00:13:02 -0700

    save

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 #
 System Event Management Software should allow users to capture system and application activity while owning their data. By collecting system and application events using a simple and cost-efficient solution, users can monitor the health of their digital assets.
 
+
+### Systemd Unit File:
+```bash
+[Unit]
+Description=Watch-Log Service
+
+[Service]
+ExecStart=bin/bash -c 'path/to/watch-log > path/to/watch-log-health 2>&1'
+WorkingDirectory=path/to
+StandardOutput=journal
+
+[Install]
+WantedBy=multi-user.target
+```
--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ use std::process::{Command, Stdio};
 struct App;
 
 impl App {
-    fn run_background() {
+    fn run_background(terminate_flag: Arc<Mutex<bool>>) -> Arc<Mutex<bool>>{
         let file_paths = vec![
             "log1.txt",
             "log2.txt",
             "log3.txt",
         ];
-        let mut count = 0;
 
         for file_path in file_paths {
             let file_path_clone = file_path.to_string();
@@ -41,55 +40,34 @@ impl App {
                     }
                 }
                 let _ = tail_process.wait();
-                count += 1;
             });
-            println!("Running background task for file: {}", file_path);
+            //println!("Running background task for file: {}", file_path);
 
         }
-
-        // TODO: Move the running task functionality here.
-        //if count == 2 {
+        thread::park();   
+        //terminate_flag
         loop {
-            println!("Loop running that listens for signals ...");
             thread::sleep(Duration::from_secs(1));
-        }
-        //}
-        //println!("App is running in the background ...");
-        // Wait for all threads to finish
-        thread::park();
-    }
-}
-fn running_task(terminate_flag: Arc<Mutex<bool>>) {
-    let arc_file = Arc::new(std::fs::File::options().append(true).create(true).open("newfile.txt").unwrap());
-    
-    loop {
-        let mut arc_file_clone = Arc::clone(&arc_file);
-        arc_file_clone.write_all(b"perform some work\n");
-        
-        thread::sleep(Duration::from_secs(1));
+            if *terminate_flag.lock().unwrap() {
+                return terminate_flag;
+            }
+        }        
 
-        if *terminate_flag.lock().unwrap() {
-            arc_file_clone.write_all(b"terminating task\n");
-            return;
-        }
     }
 }
 
-fn start_running_task() -> Arc<Mutex<bool>> {
+fn start_watcher() -> Arc<Mutex<bool>> {
     let terminate_flag = Arc::new(Mutex::new(false));
     let terminate_flag_clone = terminate_flag.clone();
 
     thread::spawn(move || {
-        running_task(terminate_flag_clone);
+        App::run_background(terminate_flag_clone);
     });
-
-    App::run_background();
 
     terminate_flag
 }
 
 fn main() {
-
     // A list, could be a vector, collection, should maintain a list of log 
     // collection tasks. Each task should continue to run while in the 
     // background.
@@ -107,9 +85,10 @@ fn main() {
     // | src_2 | ---> | dst_2 | 
     //
 
-    let terminate_flag = start_running_task();
+    let terminate_flag = start_watcher();
+    io::stdout().flush().unwrap();  
     println!("{}",std::process::id());
-   
+
     let sigquit = Arc::new(AtomicBool::new(false));
     let sigusr1 = Arc::new(AtomicBool::new(false));
     let sigusr2 = Arc::new(AtomicBool::new(false));
@@ -117,12 +96,12 @@ fn main() {
     signal_hook::flag::register(signal_hook::consts::SIGUSR1, Arc::clone(&sigusr1));
     signal_hook::flag::register(signal_hook::consts::SIGUSR2, Arc::clone(&sigusr2));
 
-    loop {
-        println!("Main function running ...");
-
+    println!("Starting watch-log ...");
+    while !*terminate_flag.lock().unwrap() {
+        io::stdout().flush().unwrap();
         if sigquit.load(Ordering::Relaxed) {
             println!("SIGQUIT signal received.");
-            break;
+            *terminate_flag.lock().unwrap() = true;
         }
         if sigusr1.load(Ordering::Relaxed) {
             println!("SIGUSR1 signal received ... Continue running.");
@@ -130,13 +109,12 @@ fn main() {
         if sigusr2.load(Ordering::Relaxed) {
             println!("SIGUSR2 signal received ... Continue running.");
         }
-
         thread::sleep(Duration::from_secs(1));
     }
 
     // terminate task gracefully
-    *terminate_flag.lock().unwrap() = true;
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(1));
+    io::stdout().flush().unwrap();
     println!("Main function terminated.");
 }
 
