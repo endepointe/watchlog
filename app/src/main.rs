04e0b71780ecb10edb267ceb7e9e1b76f3918a92
endepointe <endepointe@gmail.com>
2024-05-12 16:53:50 -0700

    arc fix

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::io::{self, Read,BufRead,BufReader,Write};
 use std::fs::{self,File, OpenOptions};
 use std::net::{TcpListener, TcpStream,Ipv4Addr, Ipv6Addr};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -15,7 +15,7 @@ use serde::Deserialize;
 struct 
 Source {
     name: String,
-    path: PathBuf
+    path: String 
 }
 
 /* Use ipv4 for now. In the future, detect the type of address.
@@ -51,52 +51,37 @@ Config {
     defaults: Defaults,
 }
 
-struct App;
+fn 
+run_tail(path: String, terminate_flag: Arc<Mutex<bool>>) -> Arc<Mutex<bool>> {
 
-impl 
-App {
-    fn run_background(terminate_flag: Arc<Mutex<bool>>) -> Arc<Mutex<bool>>{
-        let file_paths = vec![
-            "log1.txt",
-            "log2.txt",
-            "log3.txt",
-        ];
+    thread::spawn(move || {
+        let mut tail_process = Command::new("tail")
+            .args(&["-f", "-n0", "-q", &path.as_str()]).stdout(Stdio::piped()).spawn()
+            .expect("Failed to execute tail command");
 
-        for file_path in file_paths {
-            let file_path_clone = file_path.to_string();
-            thread::spawn(move || {
-                let mut tail_process = Command::new("tail")
-                    .args(&["-f", "-n0", "-q", &file_path_clone]).stdout(Stdio::piped()).spawn()
-                    .expect("Failed to execute tail command");
+        let tail_stdout = tail_process.stdout.take().unwrap();
 
-                let tail_stdout = tail_process.stdout.take().unwrap();
+        let reader = io::BufReader::new(tail_stdout);
 
-                let mut save_file = OpenOptions::new()
-                    .create(true).append(true).open(format!("{}.save", &file_path_clone))
-                    .expect("Failed to open or create .save file");
-
-                let reader = io::BufReader::new(tail_stdout);
-                for line in reader.lines() {
-                    if let Ok(line) = line {
-                        if let Err(e) = writeln!(save_file, "{}", line) {
-                            eprintln!("Failed to write to .save file: {}", e);
-                        }
-                    }
-                }
-                let _ = tail_process.wait();
-            });
-
-        }
-        thread::park();   
-        //terminate_flag
-        loop {
-            thread::sleep(Duration::from_secs(1));
-            if *terminate_flag.lock().unwrap() {
-                return terminate_flag;
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                // send to encryption
+                println!("{}", line);
+            } else {
+                // write to error log file perhaps
+                println!("Error reading line");
             }
-        }        
+        }
+        let _ = tail_process.wait();
+    });
 
-    }
+    thread::park();   
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        if *terminate_flag.lock().unwrap() {
+            return terminate_flag;
+        }
+    }        
 }
 
 fn 
@@ -114,13 +99,24 @@ start_watcher(logs: Vec<Log>) -> Arc<Mutex<bool>> {
     let terminate_flag = Arc::new(Mutex::new(false));
     let terminate_flag_clone = terminate_flag.clone();
 
-    thread::spawn(move || {
-        App::run_background(terminate_flag_clone);
-    });
+    for log in logs {
+        let arc_log = Arc::new(log);
+        let arc_log_clone = arc_log.clone();
+        let source = &arc_log_clone.source;
+        //let destination = log.destination;
+        //let compression_level = log.compression_level.unwrap_or(0);
+        //let key = log.key.unwrap_or(PathBuf::new());
+        //let tx_interval = log.tx_interval.unwrap_or(String::from("0"));
+
+        //println!("{:?} {:?} {:?} {:?} {:?}", source, destination, compression_level, key, tx_interval);
+        thread::spawn(move || {
+            run_tail(source.path.clone(), terminate_flag_clone.clone());
+        });
+        //run_tail(source.path.clone(), terminate_flag_clone.clone());
+    }
 
     terminate_flag
 }
-
 
 fn 
 main() {
